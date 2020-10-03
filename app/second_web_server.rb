483e2224da7a5ad80736bb0eec85c9495f3b520c
s4na <appletea.umauma@gmail.com>
2020-10-03 12:31:14 +0900

    Change to early return

--- app/second_web_server.rb
+++ app/second_web_server.rb
@@ -3,17 +3,18 @@ require 'socket'
 port = 3000
 server = TCPServer.open(port)
 
+# Wait for connection
 socket = server.accept
 
 loop do
   line = socket.readline.chomp
   puts "Read: #{line}"
 
-  if line.bytesize.zero?
-    socket.write "HTTP/1.1 200 OK\r\n"
-    socket.write "\r\n"
-    socket.write "Hello World\r\n"
-    socket.close
-    break
-  end
+  next unless line.bytesize.zero?
+
+  socket.write "HTTP/1.1 200 OK\r\n"
+  socket.write "\r\n"
+  socket.write "Hello World\r\n"
+  socket.close
+  break
 end
